2e8ffe3210abf4c63fa10843c48a839b9e8abad8
Paddy McGlone <paddymcglone@gmail.com>
2019-08-28 18:45:15 +0100

    Adding Author and Tags seeding

--- Queries/Migrations/Configuration.cs
+++ Queries/Migrations/Configuration.cs
@@ -1,6 +1,7 @@
 namespace Queries.Migrations
 {
     using System;
+    using System.Collections.Generic;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
@@ -14,10 +15,35 @@ namespace Queries.Migrations
 
         protected override void Seed(Queries.QueriesContext context)
         {
-            //  This method will be called after migrating to the latest version.
+            #region Tags
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data.
+            var tags = new Dictionary<string, Tag>
+            {
+                {"C#", new Tag{ Id = 1, Name = "C#"}},
+                {"JS", new Tag{ Id = 2, Name = "JS"}},
+                {"Node", new Tag{ Id = 3, Name = "Node"}}
+            };
+
+            foreach (var tag in tags.Values)
+                context.Tags.AddOrUpdate(t => t.Id, tag);
+
+            #endregion
+
+
+            #region Authors
+
+            var authors = new List<Author>()
+            {
+                new Author{ Id = 1, Name ="Patrick" },
+                new Author{ Id = 2, Name ="Tim" }
+            };
+
+            foreach (var author in authors)
+                context.Authors.AddOrUpdate(a => a.Id, author);
+
+            #endregion
+
+
         }
     }
 }
