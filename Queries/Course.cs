fd8115f687e24c71c8da3ac0fda2f4ceb71a087d
Paddy McGlone <paddymcglone@gmail.com>
2019-08-28 18:33:11 +0100

    Adding initial settings

---
+++ Queries/Course.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queries
+{
+    public class Course
+    {
+        public int Id { get; set; }
+
+        [Required, MaxLength(200)]
+        public string Title { get; set; }
+
+        //[Required]
+        public string Description { get; set; }
+
+        public CourseLevel Level { get; set; }
+
+        public float FullPrice { get; set; }
+
+        [ForeignKey(nameof(Author))] // Identify the name of annotation
+        public int AuthorId { get; set; }
+
+        public Author Author { get; set; }
+
+        public IList<Tag> Tags { get; set; }
+
+        public DateTime DatePublished { get; set; }
+    }
+}
---
+++ Queries/QueriesContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queries
+{
+    public class QueriesContext : DbContext
+    {
+        public QueriesContext()
+            : base("name=DefaultConnection")
+        {
+        }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            // Add Required attribute
+            //modelBuilder.Entity<Course>()
+            //    .Property(c => c.Description)
+            //    .IsRequired();
+        }
+    }
+}
